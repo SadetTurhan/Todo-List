f6771980e9c0649cafea184bd5823313702e7c0a
Sadet Turhan <msadetturhan1@gmail.com>
2023-01-04 19:18:47 +0300

    setting bootstrap for general layout and navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import _ from 'lodash';
-import './style.css';
-function component() {
-    const element = document.createElement('div');
+import 'bootstrap';
+function navBar() {
+  const container = document.getElementById("container");
+    container.innerHTML = 
+       '<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">' +
+      '<a href="#" class="navbar-brand">Todo List</a>' +
+      '<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">'+
+      '<span class="navbar-toggler-icon"></span>'+
+      '</button>'+
+      '<div class="collapse navbar-collapse" id="navbarCollapse">'+
+      '<ul class="navbar-nav ml-auto>'+
+      '<li class="navbar-item">'+
+      '<a href="#" class="nav-link">All tasks</a>'+
+      '</li>'+
+      '<li class="navbar-item">'+
+      '<a href="#" class="nav-link">Weekly Tasks</a>'+
+      '</li>'+
+      '<li class="navbar-item">'+
+      '<a href="#" class="nav-link">Daily tasks</a>'+
+      '</li>'+
+      '</ul>'+
+      '</div>' +
+      '</nav>'
+
+};
  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- 
-   element.classList.add('hello');
- 
-    return element;
-  }
- 
-  document.body.appendChild(component());
+navBar()
+
+export {navBar};
