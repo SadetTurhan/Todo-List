baac4018c9068559a8f746f2d16bc813599c0696
SadetTurhan <57369165+SadetTurhan@users.noreply.github.com>
2023-05-01 11:56:18 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const inputTaskName = document.getElementById('inputTaskName');
+const addButton = document.getElementById('addButton');
+const taskArea = document.getElementById('taskArea');
+
+function addTask(){
+    if(inputTaskName.value != ' '){
+    
+    const newTask = document.createElement("div");
+    newTask.classList.add("newTask");
+    const newTaskName = document.createElement("p");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add('deleteButton');
+    deleteButton.textContent = "Delete Task";
+    function deleteTask(){
+        newTask.remove() 
+    };
+    deleteButton.addEventListener("click",deleteTask);
+    newTask.textContent = inputTaskName.value;
+    inputTaskName.value = ' ';
+    newTask.appendChild(newTaskName);
+    newTask.appendChild(deleteButton);
+    taskArea.appendChild(newTask);
+    }else{
+        alert("please add a task");
+    };
+    
+};
+
+addButton.addEventListener("click",addTask);
